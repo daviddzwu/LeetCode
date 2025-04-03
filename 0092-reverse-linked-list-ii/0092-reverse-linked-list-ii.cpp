5bae2c70d31b5930b001b98f0e050fbda7dc6444
David Wu <daviddzwu@gmail.com>
2025-04-03 01:18:15 -0400

    0092-reverse-linked-list-ii.cpp

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -16,28 +16,24 @@ public:
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
 
-        // Move `prev` to the node just before the sublist
         ListNode* prev = dummy;
         for (int i = 0; i < left - 1; ++i) {
             prev = prev->next;
         }
 
-        // `last_node` will be the new tail of the reversed sublist
         ListNode* last_node = prev->next;
         ListNode* curr = prev->next;
-        ListNode* pre = nullptr; // Previous node in reversal
+        ListNode* pre = nullptr;
 
-        // Reverse the sublist between `left` and `right`
-        for (int i = 0; i <= right - left; ++i) { // Fix: Loop runs (right-left+1) times
+        for (int i = 0; i <= right - left; ++i) { 
             ListNode* next = curr->next;
             curr->next = pre;
             pre = curr;
             curr = next;
         }
 
-        // Connect the reversed sublist back to the original list
-        prev->next = pre;       // Link the node before sublist to the new head
-        last_node->next = curr; // Link the sublist's tail to the remaining nodes
+        prev->next = pre;       
+        last_node->next = curr;
 
         return dummy->next;
     }
