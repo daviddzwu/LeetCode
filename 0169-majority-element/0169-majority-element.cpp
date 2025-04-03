8f57c536a654f23a6d16a82d7a343d3e482bc931
David Wu <daviddzwu@gmail.com>
2025-04-03 18:50:46 -0400

    Time: 0 ms (100%), Space: 28.3 MB (24.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> hashTable;
+        
         for(int i = 0; i < nums.size(); i++){
-            if(hashTable.find(nums[i]) != hashTable.end()){
-                hashTable[nums[i]]++;
-            }
-            else{
-                hashTable[nums[i]] = 0;
-            }
+            hashTable[nums[i]]++;
         }
-        int maxKey = -1;
-        int maxValue = INT_MIN;
+        int n = (nums.size()) / 2;
 
-        for (const auto& pair : hashTable) {
-            if (pair.second > maxValue) {
-                maxValue = pair.second;
-                maxKey = pair.first; 
+        for (auto x : hashTable) {
+            if (x.second > n) {
+                return x.first;
             }
         }
-        return maxKey;
+        return 0;
     }
 };
