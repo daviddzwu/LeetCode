9726c74bc7344fbcc677d9f7f37d2313ba7c1bae
David Wu <daviddzwu@gmail.com>
2025-04-04 22:09:50 -0400

    Time: 3 ms (10.03%), Space: 30.6 MB (5.91%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size = nums.size();
+        k = k % nums.size();
+        vector<int> rotated(nums.size());
+        for(int i = 0; i < nums.size(); i++){
+            rotated[(i+k) % nums.size()] = nums[i];
+        }
+        for(int i = 0; i < nums.size(); i++){
+            nums[i] = rotated[i];
+        }
+    }
+};
